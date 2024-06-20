52a17dcdfa3c73d120274da41f5cbf841e14318f
txemarodsan <jose.rodriguez@stucom.com>
2024-06-20 12:44:50 +0200

    Add Atribut Diameter Ball

--- src/worldcup/Ball.java
+++ src/worldcup/Ball.java
@@ -8,6 +8,8 @@ public class Ball {
 
     /** material of which the ball is made */
     String material;
+    /** diameter of ball **/
+    Int diameter;
 
     /**
      *  Sets the ball in motion
